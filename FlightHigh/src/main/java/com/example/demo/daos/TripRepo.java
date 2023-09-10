2849e306b68948d5c43f9c00ef8427701c2a3aea
Sutirtha Mukherjee <sutirthamukherjee1@gmail.com>
2023-09-11 00:12:20 +0530

    Add files via upload

---
+++ FlightHigh/src/main/java/com/example/demo/daos/TripRepo.java
@@ -0,0 +1,13 @@
+package com.example.demo.daos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.models.Trip;
+
+public interface TripRepo extends JpaRepository<Trip,Integer>{
+	@Query("from Trip where userEmail = ?1")
+	List<Trip> findByEmail(String email);
+}
